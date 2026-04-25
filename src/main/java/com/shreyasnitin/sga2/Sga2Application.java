b77f7ced9188447db2219b45bdf4ca85d0568f2d
Shreyas Nitin <nshreyas08@gmail.com>
2026-04-25 15:25:30 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/shreyasnitin/sga2/Sga2Application.java
@@ -0,0 +1,13 @@
+package com.shreyasnitin.sga2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sga2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sga2Application.class, args);
+	}
+
+}
